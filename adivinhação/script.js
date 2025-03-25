1c1d33b2d137360e4d78c5f5aed3223273076f60
viniciusfeitoza28 <vini280207@gmail.com>
2025-03-25 17:01:57 -0300

    .

---
+++ adivinhação/script.js
@@ -0,0 +1,15 @@
+let i = Math.random() * 100;
+let x =Math.floor(i);
+
+let num = parseInt(document.getElementById(num))
+
+function tentativa(num){
+    if(num < x){
+        console.log("num" + "pequenas")
+    }
+}
+
+
+function mostratentativa(pequenas){
+    console.log(pequenas);
+}
