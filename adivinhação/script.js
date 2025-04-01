f0cda16deb301c57d861e734b60468ac0f9e73a5
viniciusfeitoza28 <vini280207@gmail.com>
2025-04-01 16:27:22 -0300

    .

--- adivinhação/script.js
+++ adivinhação/script.js
@@ -1,15 +1,24 @@
 let i = Math.random() * 100;
 let x =Math.floor(i);
+let tentavivas = 0;
+console.log(i)
+let numero = parseInt(document.getElementById(num))
 
-let num = parseInt(document.getElementById(num))
-
-function tentativa(num){
-    if(num < x){
-        console.log("num" + "pequenas")
+function guess(){
+    let numero = parseInt(document.getElementById('num').value);
+    if (numero > i){
+        document.getElementById('resp').innerHTML = "O numero é menor que" + numero;
+        document.getElementById("resp").style.setProperty("background-color","red");
     }
 }
-
-
-function mostratentativa(pequenas){
-    console.log(pequenas);
+else if (num < i ) {
+    document.getElementById ('resp').innerHTML = "O numero é maior que"+ numero;
+    document.getElementById("resp").style.setProperty("background-color","red");
+}
+tentar++
+document.getElementById('num_tentativa')"" + tentar
+if (num == 1) {
+    document.getElementById('resp').innerHTML = "Parabens,acertou!"
+    document.getElementById("resp").style.setProperty("background-color","green");
+    document.getElementById('num_tentativa').innerHTML = ""+ tentar
 }
