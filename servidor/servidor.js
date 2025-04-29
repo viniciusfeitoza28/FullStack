49d2d3584d5826cf2034ba21693c4bf3ad0188ea
viniciusfeitoza28 <vini280207@gmail.com>
2025-04-28 23:19:37 -0300

    .

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -54,5 +54,23 @@ app.get("/for_ejs",function(requisicao,resposta){
     resposta.render("exemplo_for",{valor});
 })
 
+// ----------------------------------------------------------------------------------------------------------------
+
+app.post("/cadastrolab", function(requisicao,resposta){
+    let nome = requisicao.body.nome;
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
+    let nasc = requisicao.body.nascimento;
+
+    console.log(nome,login,senha,nasc)
+
+    resposta.render("resposta",{nome,login,senha,nasc});
+    
+}) 
+
+app.get("/for_ejs",function(requisicao,resposta){
+    let valor = requisicao.query.valor;
+    resposta.render("exemplo_for",{valor});
+})
 
 
