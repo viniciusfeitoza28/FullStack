77911b8192d40443670314ee7c40ad280a21af93
viniciusfeitoza28 <vini280207@gmail.com>
2025-04-29 16:45:09 -0300

    asd

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,7 +1,15 @@
 require("colors");
 var http = require ("http");
 var express = require ("express");
+var mongodb = require("mongodb");
 let bodyParser = require("body-parser")
+
+const MongoClient = mongodb.MongoClient;
+const uri = 'mongodb+srv://viniftz:giovanna28@vini.2xzk4qv.mongodb.net/?retryWrites=true&w=majority&appName=Vini'
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("Vini");
+var usuarios = dbo.collection("usuarios");
 
 var app = express ();
 app.use(express.static('./public'));
@@ -43,9 +51,21 @@ app.post("/cadastrar", function(requisicao,resposta){
     let senha = requisicao.body.senha;
     let nasc = requisicao.body.nascimento;
 
-    console.log(nome,login,senha,nasc)
+    console.log(nome,login,senha,nasc);
 
-    resposta.render("resposta",{nome,login,senha,nasc});
+    var data = {db_nome: nome , db_login: login, db_senha: senha,db_nasci:nasc};
+
+    usuarios.insertOne(data, function(err){
+        console.log(err)
+        if(err){
+            resposta.render("resposta",{status: "Erro!" ,nome,login,senha,nasc});
+        }
+        else{
+            resposta.render("resposta",{status: "Sucesso!", nome,login,senha,nasc});
+        }
+    })
+
+    // resposta.render("resposta",{nome,login,senha,nasc});
     
 }) 
 
@@ -56,21 +76,86 @@ app.get("/for_ejs",function(requisicao,resposta){
 
 // ----------------------------------------------------------------------------------------------------------------
 
-app.post("/cadastrolab", function(requisicao,resposta){
-    let nome = requisicao.body.nome;
+var http = require('http');
+var express = require('express');
+let bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+var server = http.createServer(app);
+server.listen(80);
+
+console.log("Servidor rodando!");
+
+
+let usuarios = [];
+
+app.get("/", function(req,res){
+    res.redirect("exercicio/Home.html")
+});
+
+app.get("/cadastrar", function(req,res){
+    res.redirect("exercicio/cadastro.html")
+});
+
+app.get("/login", function(req,res){
+    res.redirect("exercicio/login.html")
+});
+
+
+app.post("/cadastrar", function(req,res){
+    let nome = req.body.Nome;
+    let user = req.body.User;
+    let senha = req.body.Senha;
+    console.log("Cadastro:", nome, user, senha);
+
+    
+    usuarios.push({nome, user, senha});
+
+    res.redirect("/login"); 
+});
+
+
+app.post("/login", function(req,res){
+    let user  = req.body.User;
+    let senha = req.body.Senha;
+    console.log("Tentativa de login:", user, senha);
+
+  
+    let encontrado = usuarios.find(u => u.user === user && u.senha === senha);
+
+    if (encontrado) {
+      
+        res.render("resposta", {nome: encontrado.nome, user: encontrado.user, senha: encontrado.senha, erro: null});
+    } else {
+       
+        res.render("resposta", {nome: null, user: null, senha: null, erro: "Usuário ou senha incorretos!"});
+    }
+});
+//--------------------
+
+app.post('/logar', function(requisicao,resposta){
     let login = requisicao.body.login;
     let senha = requisicao.body.senha;
-    let nasc = requisicao.body.nascimento;
+    console.log(login,senha);
 
-    console.log(nome,login,senha,nasc)
+    var data = {db_login: login, db_senha: senha}
 
-    resposta.render("resposta",{nome,login,senha,nasc});
-    
-}) 
-
-app.get("/for_ejs",function(requisicao,resposta){
-    let valor = requisicao.query.valor;
-    resposta.render("exemplo_for",{valor});
+    usuarios.find(data).toArray(function(err,item){
+        console.log(item)
+        if(item.length == 0){
+            resposta.render("respostalogin",{status: "usuario/senha não encontrado"})
+        }else if(err){
+            resposta.render("respostalogin",{status: "erro ao logar"})
+        }else {
+            resposta.render("respostalogin",{status: "usuario logado"})
+        }
+    })
 })
 
 
